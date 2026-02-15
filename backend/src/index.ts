36a152fb7cfb95d787a94370b739d6904ca3db55
Nguyen Huy Binh <nguyenhuybinh270709@gmail.com>
2026-02-15 12:39:29 +0700

    Initial commit

---
+++ backend/src/index.ts
@@ -0,0 +1,12 @@
+import "dotenv/config";
+import express from "express";
+
+const app = express();
+
+app.use(express.json());
+
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+  console.log(`Server in running on port ${PORT}`);
+});
