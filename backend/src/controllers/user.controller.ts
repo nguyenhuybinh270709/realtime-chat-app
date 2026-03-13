92e883f8d86e8b03eeb3c6aeaf12393e6748b6a4
Nguyen Huy Binh <nguyenhuybinh270709@gmail.com>
2026-03-13 14:12:43 +0700

    refactor(user): move updateProfile logic from controller to service

--- backend/src/controllers/user.controller.ts
+++ backend/src/controllers/user.controller.ts
@@ -1,51 +1,20 @@
-import { prisma } from "@/lib/prisma";
-import { updateProfileSchema } from "@/schema/user.schema";
-import { getUserByUsernameService } from "@/services/user.service";
+import {
+  getUserByUsernameService,
+  updateProfileService,
+} from "@/services/user.service";
 import type { NextFunction, Request, Response } from "express";
-import z from "zod";
 
-export const updateProfile = async (req: Request, res: Response) => {
+export const updateProfile = async (
+  req: Request,
+  res: Response,
+  next: NextFunction,
+) => {
   try {
-    const result = updateProfileSchema.safeParse(req.body);
-
-    if (!result.success) {
-      return res.status(400).json({
-        error: {
-          message: "Validation failed",
-          details: z.flattenError(result.error),
-        },
-      });
-    }
-
-    if (!req.user) {
-      return res.status(401).json({ error: "Unauthorized" });
-    }
-
-    const updatedUser = await prisma.user.update({
-      where: {
-        id: req.user.id,
-      },
-      data: result.data,
-      select: {
-        id: true,
-        displayName: true,
-        bio: true,
-      },
-    });
+    const updatedUser = await updateProfileService(req.body, req.user!.id);
 
     return res.status(200).json(updatedUser);
-  } catch (error: unknown) {
-    if (error instanceof Error) {
-      console.error("Error in updateProfile controller: ", error.message);
-    } else {
-      console.error("Unknown error: ", error);
-    }
-
-    return res.status(500).json({
-      error: {
-        message: "Internal server error",
-      },
-    });
+  } catch (error) {
+    next(error);
   }
 };
 
--- backend/src/services/user.service.ts
+++ backend/src/services/user.service.ts
@@ -1,5 +1,36 @@
 import { createError } from "@/errors/app.error";
 import { prisma } from "@/lib/prisma";
+import { updateProfileSchema } from "@/schema/user.schema";
+import z from "zod";
+
+export const updateProfileService = async (
+  body: unknown,
+  currentUserId: string,
+) => {
+  const parseResult = updateProfileSchema.safeParse(body);
+
+  if (!parseResult.success) {
+    throw createError(
+      400,
+      "Validation failed",
+      z.flattenError(parseResult.error),
+    );
+  }
+
+  const updatedUser = await prisma.user.update({
+    where: {
+      id: currentUserId,
+    },
+    data: parseResult.data,
+    select: {
+      id: true,
+      displayName: true,
+      bio: true,
+    },
+  });
+
+  return updatedUser;
+};
 
 export const getUserByUsernameService = async (username: string) => {
   const user = await prisma.user.findUnique({
